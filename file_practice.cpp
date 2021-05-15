ec01b0b0713f21fba5346840f435d2d1f1978e81
davtellez01 <davtellez01@gmail.com>
2021-05-15 14:10:40 -0700

    Implemented reset game

---
+++ file_practice.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main () {
+ string line;
+ ifstream myfile;
+ myfile.open("example.txt");
+ if (myfile.is_open())
+{
+while(getline(myfile,line))
+{
+if(line == "Character"){
+getline(myfile,line);
+cout << line << endl;
+}
+}
+myfile.close();
+}
+else cout << "Unable to open file" << endl;
+return 0; 
+}
--- main_test.cpp
+++ main_test.cpp
@@ -6,25 +6,13 @@
 #include "character/SB.hpp"
 #include "character/SM.hpp"
 #include "weapons/weapon.hpp"
+#include "save/save.hpp"
 
 #include <iostream>
 
 using namespace std;
 
 int main(){
-	Entity* player1 = new BCOE_Entity("Dylan", 3,1);
-   	Entity* player2 = new CHASS_Entity("David", 3,2);
-	Entity* player3 = new CNAS_Entity("Deez", 3,1);
-	Entity* player4 = new GSE_Entity("Bruh",2, 2);
-	Entity* player5 = new SB_Entity("Sheesh",7,2);
-	Entity* player6 = new SM_Entity("pp", 8, 1);
-	
-	player1->showStats();
-  	player2->showStats();
-	player3->showStats();
-	player4->showStats();
-	player5->showStats();
-	player6->showStats();
-
+	ResetGame();	
 	return 0;
 }
